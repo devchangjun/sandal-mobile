fa5159eb832830858542de5754906e407def3f99
ckdwns9121 <ckdwns9121@naver.com>
2020-08-22 15:19:51 +0900

    coupon modal

--- src/containers/order/OrderContainer.js
+++ src/containers/order/OrderContainer.js
@@ -12,7 +12,7 @@ const cx = classNames.bind(styles);
 const cp_init=[
     {
         cp_id :1,
-        event_name:"첫주문 3,000원 할인 쿠폰",
+        event_name:"첫 주문 3,000원 할인 쿠폰",
         sale :"3000원",
         sub_name:"첫주문시 사용",
         date:"2020-05-10 ~ 2020-06-10 까지",
@@ -21,34 +21,34 @@ const cp_init=[
 
     {
         cp_id :2,
-        event_name:"첫주문 3,000원 할인 쿠폰",
+        event_name:"첫 주문 3,000원 할인 쿠폰",
         sale :"4000원",
-        sub_name:"첫주문시 사용",
+        sub_name:"첫 주문시 사용",
         date:"2020-05-10 ~ 2020-06-10 까지",
         select : false,
     },
     
     {
         cp_id :3,
-        event_name:"첫주문 3,000원 할인 쿠폰",
+        event_name:"첫 주문 3,000원 할인 쿠폰",
         sale :"4000원",
-        sub_name:"첫주문시 사용",
+        sub_name:"첫 주문시 사용",
         date:"2020-05-10 ~ 2020-06-10 까지",
         select : false,
     },
     {
         cp_id :4,
-        event_name:"첫주문 3,000원 할인 쿠폰",
+        event_name:"첫 주문 3,000원 할인 쿠폰",
         sale :"4000원",
-        sub_name:"첫주문시 사용",
+        sub_name:"첫 주문시 사용",
         date:"2020-05-10 ~ 2020-06-10 까지",
         select : false,
     },
     {
         cp_id :5,
-        event_name:"첫주문 3,000원 할인 쿠폰",
+        event_name:"첫 주문 3,000원 할인 쿠폰",
         sale :"4000원",
-        sub_name:"첫주문시 사용",
+        sub_name:"첫 주문시 사용",
         date:"2020-05-10 ~ 2020-06-10 까지",
         select : false,
     }

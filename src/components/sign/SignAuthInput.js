0dc409a867727f391b4817e1906f1003fe4b3aeb
박철현 <parkcheolhyun@cheolmaegi.local>
2020-08-23 18:06:19 +0900

    Connect SOLAPI

--- src/components/sign/SignAuthInput.js
+++ src/components/sign/SignAuthInput.js
@@ -4,17 +4,41 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-
 //인증 버튼을 포함한 input box
 
-const SignAuthInput = ({ inputType, onChange, onClick, initValue ,buttonTitle ,placeholder,toggle, success}) => {
+const SignAuthInput = ({
+    inputType,
+    onChange,
+    onClick,
+    initValue,
+    buttonTitle,
+    placeholder,
+    toggle,
+    success,
+}) => {
     return (
         <div className={styles['sign-input']}>
             <form>
-            <input className={styles['auth']} type={inputType} value={initValue}  placeholder={placeholder} onChange={onChange}  autoComplete="on"></input>
+                <input
+                    className={styles['auth']}
+                    type={inputType}
+                    value={initValue}
+                    placeholder={placeholder}
+                    onChange={onChange}
+                    autoComplete="on"
+                ></input>
             </form>
-            <div className={cx('auth-btn',{toggle:toggle},{success:success})} onClick={onClick}>{buttonTitle}</div>
+            <div
+                className={cx(
+                    'auth-btn',
+                    { toggle: toggle },
+                    { success: success },
+                )}
+                onClick={onClick}
+            >
+                {buttonTitle}
+            </div>
         </div>
-    )
-}
+    );
+};
 export default SignAuthInput;
--- src/containers/sign/RecoveryPwContainer.js
+++ src/containers/sign/RecoveryPwContainer.js
@@ -5,47 +5,69 @@ import SignAuthInput from 'components/sign/SignAuthInput';
 import TitleBar from 'components/titlebar/TitleBar';
 import Button from 'components/button/Button';
 import classNames from 'classnames/bind';
+import { sendSMS } from "../../api/sms/sms";
 
-const cx= classNames.bind(styles);
+const cx = classNames.bind(styles);
 
-const logo = "http://www.agenciasampling.com.br/asampling/assets/img/sample/shortcode/logo/1.png";
+const logo =
+    'http://www.agenciasampling.com.br/asampling/assets/img/sample/shortcode/logo/1.png';
 
 const RecoveryPwContainer = () => {
-
-
     const [name, setName] = useState('');
-    const [email ,setEmail] = useState('');
+    const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [auth, setAuth] = useState('');
-    const [toggle , setToggle] = useState(false);
-    // const [success , setSuccess] = useState(false); 
+    const [toggle, setToggle] = useState(false);
+    // const [success , setSuccess] = useState(false);
 
-    const onChangeName=e=>setName(e.target.value);
-    const onChangeEmail = e => setEmail(e.target.value);
-    const onChangePhone = e => setPhone(e.target.value);
-    const onChangeAuth = e => setAuth(e.target.value);
-    const onChangePostAuthNumber =e=>setToggle(true);
-    
-
-
+    const onChangeName = (e) => setName(e.target.value);
+    const onChangeEmail = (e) => setEmail(e.target.value);
+    const onChangePhone = (e) => setPhone(e.target.value);
+    const onChangeAuth = (e) => setAuth(e.target.value);
+    const onChangePostAuthNumber = (e) => {
+        sendSMS('01094873263', 'TEST');
+        console.log('GOGO');
+        setToggle(true);
+    }
     return (
         <>
-            <TitleBar title="비밀번호 찾기" src={logo} alt="비밀번호 찾기"></TitleBar>
+            <TitleBar title="비밀번호 찾기" src={logo} alt="비밀번호 찾기" />
             <div className={styles['sign-main']}>
                 <div className={styles['sign-content']}>
-                    <SignNormalInput placeholder={"이름"} initValue={name} onChange={onChangeName}/>
-                    <SignNormalInput placeholder={"이메일"} initValue={email} onChange={onChangeEmail}/>
-                    <SignAuthInput placeholder={"휴대폰번호"}inputType={""} initValue={phone} buttonTitle={toggle ? "인증번호 재발송" : "인증번호 발송"} onChange={onChangePhone} onClick={onChangePostAuthNumber}  toggle={toggle}/>
-                    <div className={cx('auth-btn',{not_view: !toggle})}>
-                    <SignAuthInput inputType={"text"} initValue={auth} buttonTitle={"인증하기"} onChange={onChangeAuth} />
+                    <SignNormalInput
+                        placeholder={'이름'}
+                        initValue={name}
+                        onChange={onChangeName}
+                    />
+                    <SignNormalInput
+                        placeholder={'이메일'}
+                        initValue={email}
+                        onChange={onChangeEmail}
+                    />
+                    <SignAuthInput
+                        placeholder={'휴대폰번호'}
+                        inputType={''}
+                        initValue={phone}
+                        buttonTitle={
+                            toggle ? '인증번호 재발송' : '인증번호 발송'
+                        }
+                        onChange={onChangePhone}
+                        onClick={onChangePostAuthNumber}
+                        toggle={toggle}
+                    />
+                    <div className={cx('auth-btn', { not_view: !toggle })}>
+                        <SignAuthInput
+                            inputType={'text'}
+                            initValue={auth}
+                            buttonTitle={'인증하기'}
+                            onChange={onChangeAuth}
+                        />
                     </div>
-                    <Button title={"확인"}></Button>
-
+                    <Button title={'확인'}></Button>
                 </div>
             </div>
         </>
-    )
-}
-
+    );
+};
 
 export default RecoveryPwContainer;

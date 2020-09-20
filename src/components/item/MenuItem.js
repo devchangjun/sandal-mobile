c2a4b11644a1b9d617aa6e0c0cbdb257b9404699
박창준 <ckdwns9121@naver.com>
2020-09-20 21:45:22 +0900

    # 메뉴 탭 잘 안옮겨지는거 수정

--- src/components/item/MenuItem.js
+++ src/components/item/MenuItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './MenuItem.module.scss';
 import IMG from '../svg/menu/menuitem.png';
 import classNames from 'classnames/bind';
-import { ButtonBase } from '@material-ui/core';
 import { numberFormat } from "../../lib/formatter";
 
 const cx = classNames.bind(styles);
@@ -12,9 +11,9 @@ const MenuItem = (props) => {
     return (
 
         <div className={styles['menu-item']} onClick={props.onClick}>
-            <ButtonBase className={styles['menu-img']}>
+            <div className={styles['menu-img']}>
                 <img src={IMG} className={styles['img']} alt="메뉴" />
-            </ButtonBase>
+            </div>
             <div className={cx('menu-info','pd-box')}>
                 <div className={styles['menu-name']}>
                    {props.item_name}

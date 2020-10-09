d3ba8cdbe52489288d031592ee9b34b2834fe9b8
박철현 <parkcheolhyun@cheolmaegi.local>
2020-10-09 14:51:29 +0900

    # index.html 정리

--- src/index.js
+++ src/index.js
@@ -3,32 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import createSagaMiddleware from 'redux-saga';
-import rootReducer,{rootSaga} from './store';
-import { createStore,applyMiddleware } from 'redux';
-import {createLogger} from 'redux-logger';
+import rootReducer, { rootSaga } from './store';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware))
+    rootReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
 
 sagaMiddleware.run(rootSaga);
-  
-ReactDOM.render(
-  <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </Provider>,
 
-  document.getElementById('root')
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+
+    document.getElementById('ajoonamy'),
 );
 
 // If you want your app to work offline and load faster, you can change

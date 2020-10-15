5f84f1616076179521b9b2926cd6e0b7063b7706
박철현 <parkcheolhyun@cheolmaegi.local>
2020-10-16 08:26:34 +0900

    # 달력 크로스 브라우징

--- src/lib/calculateDate.js
+++ src/lib/calculateDate.js
@@ -5,20 +5,23 @@ export const calculateDate = (date, term, type) => {
             return new Date(cal_date.setDate(cal_date.getDate() - term));
         case 'MONTH':
             return new Date(cal_date.setMonth(cal_date.getMonth() - term));
-        case 'YEAR' :
-            return new Date(cal_date.setFullYear(cal_date.getFullYear() - term));
+        case 'YEAR':
+            return new Date(
+                cal_date.setFullYear(cal_date.getFullYear() - term),
+            );
         default:
             return cal_date;
     }
-}
+};
 
-export const calculateDay =(date) =>{
+export const calculateDay = (date) => {
+    date = date.replace(/-/gi, '/'); // '2019/09/09 17:22'
     var newArr = date.split(' ');
+    const week = ['(일)', '(월)', '(화)', '(수)', '(목)', '(금)', '(토)'];
 
-    const week = new Array('(일)','(월)','(화)','(수)','(목)','(금)','(토)');
     const cal_date = new Date(date);
     const day = week[cal_date.getDay()];
     const str = newArr.join(` ${day} `);
 
     return str;
-}
+};

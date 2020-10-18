07f6cf68c7f5be203000c1513d30b39d04f5761d
박철현 <parkcheolhyun@cheolmaegi.local>
2020-10-18 21:12:01 +0900

    # 견적서 수정

--- src/components/modal/EstmModal.js
+++ src/components/modal/EstmModal.js
@@ -1,7 +1,7 @@
 import React, { useReducer, useCallback, useState } from 'react';
 import classNames from 'classnames/bind';
 import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
+import 'jspdf-autotable';
 import { requestPostEstimate } from '../../api/order/estimate';
 
 import Estimate from '../asset/Estimate';
@@ -21,6 +21,10 @@ import styles from './EstmModal.module.scss';
 import { isEmailForm } from '../../lib/formatChecker';
 import { useModal } from '../../hooks/useModal';
 import Loading from '../asset/Loading';
+import SDGothicNEO_R from './AppleSDGothicNeoR';
+import SDGothicNEO_B from './AppleSDGothicNeoB';
+import { useSelector } from 'react-redux';
+
 
 const cx = classNames.bind(styles);
 
@@ -87,6 +91,9 @@ const EstmModal = (props) => {
     const classes = useStyles();
     const openModal = useModal();
 
+    const { company } = useSelector(state => state.company);
+    const { com_name, addr1, addr2, ceo_name, tel, business_num } = company;
+
     const [loading, setLoading] = useState(false);
     const [state, dispatch] = useReducer(reducer, {
         receiver: '',
@@ -125,35 +132,63 @@ const EstmModal = (props) => {
         }
     }, [estmFile, state, openModal, props]);
 
-    const onDownload = (ref) => {
-        let position = 0;
+    const onDownload = () => {
         const doc = new jsPDF('p', 'mm');
-        window.scrollTo(0, 0);
-        setLoading(true);
-        html2canvas(ref.current).then((canvas) => {
-            const imageData = canvas.toDataURL('image/png');
-            const imgWidth = 210; // 이미지 가로 길이(mm) A4 기준
-            const pageHeight = imgWidth * 1.414; // 출력 페이지 세로 길이 계산 A4 기준
-            const imgHeight = (canvas.height * imgWidth) / canvas.width;
-            let heightLeft = imgHeight;
-            // doc.addImage(imageData, 'PNG', 0, 0, 210, 297);
-            doc.addImage(imageData, 'PNG', 0, position, imgWidth, imgHeight, undefined, 'slow');
-            // doc.addImage(imageData, 'PNG', 0, position, canvas.width, canvas.height);
-            heightLeft -= pageHeight;
-            while (heightLeft >= 20) {
-                position = heightLeft - imgHeight;
-                doc.addPage();
-                doc.addImage(imageData, 'PNG', 0, position, imgWidth, imgHeight);
-                heightLeft -= pageHeight;
-            }
-            const blob = doc.output('blob');
-            const makeFile = new File([blob], '샌달 견적서.pdf', {
-                type: blob.type,
-            });
-            window.open(doc.output('bloburl'));
-            setEstmFile(makeFile);
-            setLoading(false);
+        doc.addFileToVFS('AppleSDGothicNeoR.ttf', SDGothicNEO_R);
+        doc.addFileToVFS('AppleSDGothicNeoB.ttf', SDGothicNEO_B);
+        doc.addFont('AppleSDGothicNeoR.ttf', 'apple', 'normal');
+        doc.addFont('AppleSDGothicNeoB.ttf', 'apple', 'bold');
+        doc.setFont('apple');
+        doc.autoTable({
+            theme: 'plain',
+            styles: {
+                font: 'apple',
+                fontSize: '30',
+                fontStyle: 'bold',
+                halign: 'center'
+            },
+            body: [['견적서']]
+        })
+        doc.autoTable({
+            theme: 'plain',
+            styles: {
+                font: 'apple',
+                fontStyle: 'normal',
+                lineWidth: 0.5,
+                lineColor: '#222',
+                overflow: 'linebreak',
+                halign: 'right',
+                valign: 'middle'
+            },
+            headStyles: {
+                halign: 'center'
+            },
+            html: '#estimate-table',
         });
+        doc.autoTable({
+            theme: 'plain',
+            styles: {
+                font: 'apple',
+                fontStyle: 'normal',
+                halign: 'center',
+                fontSize: '12',
+            },
+            footStyles: {
+                fontSize: '24',
+                fontStyle: 'bold',
+            },
+            margin: { top: 100 },
+            body: [['상기와 같이 견적서를 제출합니다.']],
+            foot: [['샌달 드림']]
+        })
+        
+        const blob = doc.output('blob');
+        const makeFile = new File([blob], '샌달 견적서.pdf', {
+            type: blob.type,
+        });
+        window.open(doc.output('bloburl'));
+        setEstmFile(makeFile);
+        setLoading(false);
     };
 
     return (
@@ -204,7 +239,15 @@ const EstmModal = (props) => {
                 className={`${classes.content} ${classes.estimate}`}
             >
                 <ButtonBase className={classes.estimatePreview}>
-                <Estimate onDownload={onDownload} products={props.cartList} dlvCost={props.dlvCost} />
+                <Estimate
+                    receiver={state.receiver}
+                    com_name={com_name} ceo_name={ceo_name}
+                    address={addr1 + ' ' + addr2} business_num={business_num}
+                    tel={tel}
+                    onDownload={onDownload}
+                    products={props.cartList}
+                    dlvCost={props.dlvCost}
+                />
                 </ButtonBase>
             </DialogContent>
             <LinkButton

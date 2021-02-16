e4acaf38742191dc974fd14134a8a04ba58b869c
ckdwns9121 <ckdwns9121@naver.com>
2021-02-16 15:06:49 +0900

    # 페이플 운영키

--- src/containers/order/OrderContainer.js
+++ src/containers/order/OrderContainer.js
@@ -327,7 +327,7 @@ const OrderContainer = ({ modal }) => {
     }
 
     const onClickOrder = async () => {
-        const payple_url = 'https://testcpay.payple.kr/js/cpay.payple.1.0.1.js';
+        const payple_url = 'https://cpay.payple.kr/js/cpay.payple.1.0.1.js';
 
         const year = date.getFullYear();
         const month = date.getMonth()+1 > 9 ? date.getMonth()+1 : `0${date.getMonth()+1}`;
